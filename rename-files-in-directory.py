55d3aa8d6eef8e0d123ca2144c04859382f59c7c
artwilton <Arthur.M.Wilton@gmail.com>
2021-04-01 01:17:43 -0400

    finished and tested script

--- rename-files-in-directory.py
+++ rename-files-in-directory.py
@@ -1,13 +1,23 @@
-# This script ignores hidden files and directories
+# This script looks at files at the base level of the current directory and renames them.
+# It ignores hidden files and directories.
 
+import re
 from pathlib import Path
 
-directory = 'directory/name/here'
+files = Path.cwd().iterdir()
 
-def rename(file):
-    print(file.name)
+def rename(file_name):
 
-files = Path(directory).iterdir()
+    #seperate file stem and extension (suffix) for easier editing
+    file_ext = file_name.suffix
+    file_stem = file_name.stem
+
+    #add your regex here
+    replace_white_space = re.sub('\s', '_', file_stem)
+    remove_special_characters = re.sub('\W', '', replace_white_space)
+    final_file_name = remove_special_characters + file_ext
+
+    Path(file_name).rename(final_file_name)
 
 for file_name in files:
     if (file_name.is_file() and not file_name.name.startswith('.')):
