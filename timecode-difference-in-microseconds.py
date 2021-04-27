8a857dfff26bb3c0b6be1651cae332e68587a719
artwilton <Arthur.M.Wilton@gmail.com>
2021-04-27 02:16:04 -0400

    syntax cleanup, imported datetime module

--- timecode-difference-in-microseconds.py
+++ timecode-difference-in-microseconds.py
@@ -1,12 +1,14 @@
 """
-This script calculates the difference between two timecode entries in microseconds.
+This script calculates the difference between two timecode entries in seconds, accurate to the microsecond.
 Timecode is assumed to be in the following format HH:MM:SS.MICROSECONDS
 """
 
-start_time = "HH:MM:SS.MICROSECONDS"
-end_time = "HH:MM:SS.MICROSECONDS"
+import datetime
 
-calculateDifference(start, end):
+start_time = "HH:MM:SS.microseconds"
+end_time = "HH:MM:SS.microseconds"
+
+def calculateDifference(start, end):
     
     def decimalCheck(time):
         if '.' not in time:
@@ -15,4 +17,8 @@ calculateDifference(start, end):
 
     start = datetime.datetime.strptime(decimalCheck(start), "%H:%M:%S.%f")
     end = datetime.datetime.strptime(decimalCheck(end), "%H:%M:%S.%f")
-    return (end - start).microseconds/1000000
+    final_time = (end - start).total_seconds()
+    
+    print (final_time)
+
+calculateDifference(start_time, end_time)
