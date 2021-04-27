3bc5462815291474e3b4a7eea73c19bdd992f149
artwilton <Arthur.M.Wilton@gmail.com>
2021-04-26 23:59:45 -0400

    added ideas for more functionality

--- run-ffmpeg-from-python.py
+++ run-ffmpeg-from-python.py
@@ -1,6 +1,8 @@
 ## This script uses the subprocess module to run custom ffmpeg commands from a python script
 ## useful when you need to run complex commands and want something more flexible than a shell script
 
+## A tip for making this script more useful would be to add more variables for things like the output codecs for video and audio, preset speed, crf rate, output resolution, etc. and then building a prompt to grab user input, passing the result to those variables
+
 import subprocess
 
 ## replace this with path for your ffmpeg binary if you're not using homebrew on MacOS
