9bc8f9610438a7d0f630650571af5b5135b7e9c4
artwilton <Arthur.M.Wilton@gmail.com>
2021-04-26 23:32:56 -0400

    set up global variables

--- run-ffmpeg-from-python.py
+++ run-ffmpeg-from-python.py
@@ -5,3 +5,10 @@ import subprocess
 
 ## replace this with path for your ffmpeg binary if you're not using homebrew on MacOS
 ffmpeg = "/usr/local/bin/ffmpeg"
+
+## ffmpeg commands to use, commands should each be a string entered in a list, example command:
+## [ffmpeg, "-i", input_file, "-c:v", "libx264", "-preset", "fast", "crf", "22", "-s", "1280x720", "-c:a", "libfdk_aac", "-b:a", "196k", "-ar", "44100", "-pix_fmt", "yuv420p", output_file]
+commands_list = []
+
+input_file = "/path/to/input/file"
+output_file = "path/to/desired/output/output_file.ext"
