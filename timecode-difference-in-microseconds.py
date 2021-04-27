1917b563b4c5719061b81f0c217575bdddd1ad6c
artwilton <Arthur.M.Wilton@gmail.com>
2021-04-27 01:48:30 -0400

    added helper script decimalCheck() for proper formatting

--- timecode-difference-in-microseconds.py
+++ timecode-difference-in-microseconds.py
@@ -5,3 +5,10 @@ Timecode is assumed to be in the following format HH:MM:SS.MICROSECONDS
 
 start_time = "HH:MM:SS.MICROSECONDS"
 end_time = "HH:MM:SS.MICROSECONDS"
+
+calculateDifference(start, end):
+    
+    def decimalCheck(time):
+        if '.' not in time:
+            time = time + '.0'
+        return time
