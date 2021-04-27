72dddc054d82330e9b7ead8b10452349a5fddc7e
artwilton <Arthur.M.Wilton@gmail.com>
2021-04-27 01:46:27 -0400

    initial variable setup

---
+++ timecode-difference-in-microseconds.py
@@ -0,0 +1,7 @@
+"""
+This script calculates the difference between two timecode entries in microseconds.
+Timecode is assumed to be in the following format HH:MM:SS.MICROSECONDS
+"""
+
+start_time = "HH:MM:SS.MICROSECONDS"
+end_time = "HH:MM:SS.MICROSECONDS"
