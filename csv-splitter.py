caf6d644a0327f4a654712715edcc94cb5d49644
artwilton <Arthur.M.Wilton@gmail.com>
2021-05-05 13:30:37 -0400

    setup basic csv reader test

--- csv-splitter.py
+++ csv-splitter.py
@@ -6,5 +6,16 @@ row_split_amount = 10000
 with open(csv_file_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
 
+    column_headers = []
+  
+    # loop to iterate thorugh the rows of csv
     for row in csv_reader:
-        print(row)
+  
+        # adding the first row
+        column_headers.append(row)
+  
+        # breaking the loop after the
+        # first iteration itself
+        break
+
+    print(column_headers)
