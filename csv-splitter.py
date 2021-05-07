5f4a8141984571a2cd6c5b362f53d118c5da873f
artwilton <Arthur.M.Wilton@gmail.com>
2021-05-07 01:36:46 -0400

    basic version of script working, just need to clean up functions a bit

--- csv-splitter.py
+++ csv-splitter.py
@@ -3,27 +3,41 @@ import csv
 csv_file_path = "/Users/arthur/Desktop/csv_test.csv"
 split_threshold = int(input('Enter how many rows per CSV file you would like: '))
 
-def loopThroughCsv(csv):
+def grabColumnHeaders(row):
     column_headers = []
+    for key in row.keys():
+        column_headers.append(key)
+    
+    return column_headers
+
+def writeNewCsv(csv_file, column_headers, split_number):
+
+    writer = csv.DictWriter(csv_file, fieldnames=column_headers)
+    writer.writeheader()
+
+    return writer
+
+def loopThroughCsv(csv):
+
     row_counter = 0
-    split_number = 1
+    split_number = 0
 
     for row in csv:
 
-        if row_counter == 0:
-            column_headers = row
-            print(f"column headers = {column_headers}")
+        column_headers = grabColumnHeaders(row)
+
+        if (row_counter == 0) or (row_counter == split_threshold):
+            split_number += 1
+            write_csv = open(f'CSV_{split_number}.csv', 'w')
+            writer = writeNewCsv(write_csv, column_headers, split_number)
+            writer.writerow(row)
+
+            row_counter = 0
         else:
-            if row_counter > split_threshold:
-                split_number += 1
-                print(f"write new row to csv{split_number}")
-                row_counter = 1
-            else:
-                print(f"write new row to csv{split_number}")
+            writer.writerow(row)
 
         row_counter += 1
 
 with open(csv_file_path) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-
+    csv_reader = csv.DictReader(csv_file)
     loopThroughCsv(csv_reader)
