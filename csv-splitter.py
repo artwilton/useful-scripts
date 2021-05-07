2242d67e703e7289299eb1a9b6aec9269113ca3b
artwilton <Arthur.M.Wilton@gmail.com>
2021-05-07 02:10:53 -0400

    created main() function and refactored grabbing column headers by simply using the fieldnames attribute on the DictReader obj

--- csv-splitter.py
+++ csv-splitter.py
@@ -3,41 +3,38 @@ import csv
 csv_file_path = "/Users/arthur/Desktop/csv_test.csv"
 split_threshold = int(input('Enter how many rows per CSV file you would like: '))
 
-def grabColumnHeaders(row):
-    column_headers = []
-    for key in row.keys():
-        column_headers.append(key)
-    
-    return column_headers
+def writeNewCsv(csv_file_write, column_headers, split_number):
 
-def writeNewCsv(csv_file, column_headers, split_number):
-
-    writer = csv.DictWriter(csv_file, fieldnames=column_headers)
+    writer = csv.DictWriter(csv_file_write, fieldnames=column_headers)
     writer.writeheader()
 
     return writer
 
-def loopThroughCsv(csv):
+def loopThroughCsv(csv, column_headers):
 
     row_counter = 0
     split_number = 0
 
     for row in csv:
 
-        column_headers = grabColumnHeaders(row)
-
         if (row_counter == 0) or (row_counter == split_threshold):
             split_number += 1
             write_csv = open(f'CSV_{split_number}.csv', 'w')
             writer = writeNewCsv(write_csv, column_headers, split_number)
             writer.writerow(row)
-
             row_counter = 0
         else:
             writer.writerow(row)
 
         row_counter += 1
 
-with open(csv_file_path) as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    loopThroughCsv(csv_reader)
+def main():
+    with open(csv_file_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        column_headers = csv_reader.fieldnames
+        loopThroughCsv(csv_reader, column_headers)
+
+if __name__ == "__main__":
+    main()
+
+## need to figure out logic to close file
