3286819f5c08d2d991a0583dc5cb0c842ddfeb2e
artwilton <Arthur.M.Wilton@gmail.com>
2021-06-10 04:55:24 -0400

    added timecode overlay that matches for NDF or DF

--- generate-test-videos.sh
+++ generate-test-videos.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## This script generates sample video files that contain HD Bars and Tone
+## Variables are included at the top of the script for quick customization
+
 video_duration="10"
 font_size="50"
 font_family_path="/Library/Fonts/Arial.ttf"
@@ -16,10 +19,13 @@ for frame_rate in ${frame_rate_array[@]}; do
     # removes the '.' and '_NDF' or '_DF' from frame_rate string for proper formatting in ffmpeg
     frame_rate_name=${frame_rate//.}
     frame_rate=$(sed 's/_.*//' <<< $frame_rate)
+    # formats timecode for ffmpeg filter, ex: '00:00:00:00' becomes '00\:00\:00\:00'
+    escaped_timecode=$(sed 's/:/\\:/g' <<< $start_timecode)
 
     ffmpeg -f lavfi -i smptehdbars=duration=$video_duration:size=1280x720:rate=$frame_rate \
     -f lavfi -i sine=frequency=1000:sample_rate=48000:duration=$video_duration \
-    -vf "[in]drawtext=fontfile=$font_family_path:fontsize=$font_size:text='Frame\: %{frame_num}':start_number=0:x=(w-tw)/2:y=h-(2*lh):fontcolor=white:box=1:boxcolor=black:boxborderw=4, drawtext=fontfile=$font_family_path:fontsize=$font_size:'timecode=00\:00\:00\:00':rate=$frame_rate:x=(w-tw)/2:y=h-(2*lh)-$font_size:fontcolor=white:box=1:boxcolor=black:boxborderw=4[out]" \
+    -vf "[in]drawtext=fontfile=$font_family_path:fontsize=$font_size:text='Frame\: %{frame_num}':start_number=0:x=(w-tw)/2:y=h-(2*lh):fontcolor=white:box=1:boxcolor=black:boxborderw=4, drawtext=fontfile=$font_family_path:fontsize=$font_size:'timecode=$escaped_timecode':rate=$frame_rate:x=(w-tw)/2:y=h-(2*lh)-$font_size:fontcolor=white:box=1:boxcolor=black:boxborderw=4[out]" \
     -c:v libx264 -pix_fmt yuv420p -preset ultrafast -crf 22 -c:a aac \
     -timecode $start_timecode "${frame_rate_name}_fps_sample_video".mp4
+
 done
