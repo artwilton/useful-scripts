4e68016b387fd97cffa7947a61929a6e88c85d4e
artwilton <Arthur.M.Wilton@gmail.com>
2021-06-10 13:49:49 -0400

    cleaned up code

--- generate-test-videos.sh
+++ generate-test-videos.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-## This script generates sample video files that contain HD Bars and Tone
+## This script generates h.264 video files that contain HD Bars and Tone using FFmpeg and overlays the frame count and timecode data
 ## Variables are included at the top of the script for quick customization
 
-video_duration="10"
+
+video_duration="10" #duration is in seconds
 font_size="50"
 font_family_path="/Library/Fonts/Arial.ttf"
 frame_rate_array=("23.976" "24" "25" "29.97_NDF" "29.97_DF" "30" "50" "59.94_NDF" "59.94_DF" "60")
@@ -24,7 +25,10 @@ for frame_rate in ${frame_rate_array[@]}; do
 
     ffmpeg -f lavfi -i smptehdbars=duration=$video_duration:size=1280x720:rate=$frame_rate \
     -f lavfi -i sine=frequency=1000:sample_rate=48000:duration=$video_duration \
-    -vf "[in]drawtext=fontfile=$font_family_path:fontsize=$font_size:text='Frame\: %{frame_num}':start_number=0:x=(w-tw)/2:y=h-(2*lh):fontcolor=white:box=1:boxcolor=black:boxborderw=4, drawtext=fontfile=$font_family_path:fontsize=$font_size:'timecode=$escaped_timecode':rate=$frame_rate:x=(w-tw)/2:y=h-(2*lh)-$font_size:fontcolor=white:box=1:boxcolor=black:boxborderw=4[out]" \
+    -vf "[in]drawtext=fontfile=$font_family_path:fontsize=$font_size:text='Frame\: %{frame_num}':start_number=0: \
+    x=(w-tw)/2:y=h-(2*lh):fontcolor=white:box=1:boxcolor=black:boxborderw=4, \
+    drawtext=fontfile=$font_family_path:fontsize=$font_size:'timecode=$escaped_timecode':rate=$frame_rate: \
+    x=(w-tw)/2:y=h-(2*lh)-$font_size:fontcolor=white:box=1:boxcolor=black:boxborderw=4[out]" \
     -c:v libx264 -pix_fmt yuv420p -preset ultrafast -crf 22 -c:a aac \
     -timecode $start_timecode "${frame_rate_name}_fps_sample_video".mp4
 
